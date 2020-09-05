1be77a8d255e31ed3254be2e50bf1cb281c7b63c
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-09-05 19:31:46 -0300

    Import fonts

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
 const SingIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
+    <Title> Faz seu login</Title>
   </Container>
 );
 
