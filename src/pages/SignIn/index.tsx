3a7ddd58534d325593ef0bdfdfd7d775bd467b48
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-09-16 23:12:22 -0300

    Commit to rollback for try fix a bug

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import {
   Image,
   KeyboardAvoidingView,
@@ -9,8 +9,12 @@ import {
 
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 import logoImg from '../../assets/logo.png';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import {
   Container,
   Title,
@@ -19,11 +23,13 @@ import {
   CreateAccountButton,
   CreateAccountButtonText,
 } from './styles';
-import Input from '../../components/Input';
-import Button from '../../components/Button';
 
 const SingIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
+  const handleSignIn = useCallback((data: any) => {
+    console.log(data);
+  }, []);
   return (
     <>
       <KeyboardAvoidingView
@@ -37,20 +43,24 @@ const SingIn: React.FC = () => {
         >
           <Container>
             <Image source={logoImg} />
+
             <View>
               <Title>Faça seu logon</Title>
             </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input name="email" icon="mail" placeholder="E-mail" />
 
-            <Button
-              onPress={() => {
-                console.log('Deu');
-              }}
-            >
-              Entrar
-            </Button>
+              <Input name="password" icon="lock" placeholder="Senha" />
+
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Entrar
+              </Button>
+            </Form>
 
             <ForgotPassword onPress={() => navigation.navigate('signUp')}>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
